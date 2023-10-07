96eebf2db15309900ae40743ba56dd7e9e2e66b5
Janhvi-S103 <Janhvis103@gmail.com>
2023-10-07 15:48:39 +0530

    C++ program to print all duplicate elements in an array

---
+++ findduplicate.cpp
@@ -0,0 +1,35 @@
+// C++ program to print all elements that
+// appear more than once.
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> duplicates(int arr[], int n)
+{
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        int index = arr[i] % n;
+        arr[index] += n;
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        if ((arr[i] / n) >= 2)
+            ans.push_back(i);
+    }
+    return ans;
+}
+
+int main()
+{
+    int arr[] = { 10,2,6,2,10,2,2,2,9,7,7,8 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    cout << "The repeating elements are: \n";
+    vector<int> ans = duplicates(arr, arr_size);
+    for (auto x : ans)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
