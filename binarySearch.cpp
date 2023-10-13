2302299a0b3b342430714575dc43764065560b73
Arpita Jadhav <89975545+arpitajadhav155@users.noreply.github.com>
2023-10-13 22:08:18 +0530

    Add files via upload

---
+++ binarySearch.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+//array must be sorted for binary search
+int binarySearch(int n, int arr[],int key){
+    int s=0;
+    int e=n;
+    while (s<=e)
+    {
+        int mid=(s+e)/2;
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if (arr[mid]>key)
+        {
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    cout<<binarySearch(n,arr,key);
+    return 0;
+}
