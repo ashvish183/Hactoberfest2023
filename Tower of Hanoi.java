ea7694a2b67bd2308491c5dd0844baaa3272dcc2
Oscar Kandir <110010835+OscarK-coder@users.noreply.github.com>
2023-10-15 14:00:53 +0000

    Tower of Hanoi

---
+++ Tower of Hanoi.java
@@ -0,0 +1,17 @@
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n, char source, char auxiliary, char target) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + target);
+            return;
+        }
+
+        towerOfHanoi(n - 1, source, target, auxiliary);
+        System.out.println("Move disk " + n + " from " + source + " to " + target);
+        towerOfHanoi(n - 1, auxiliary, source, target);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        towerOfHanoi(n, 'A', 'B', 'C');
+    }
+}
